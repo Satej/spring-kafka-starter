a81df87849d5672df99d44a658869032a719acaa
Satej <Satej>
2023-01-28 16:09:00 +0000

    Created kafka producer

--- src/main/java/io/confluent/developer/spring/SpringCcloudApplication.java
+++ src/main/java/io/confluent/developer/spring/SpringCcloudApplication.java
@@ -11,3 +11,24 @@ public class SpringCcloudApplication {
 	}
 
 }
+
+ @RequiredArgsConstructor
+ @Component
+ class Producer {
+
+ 	private final KafkaTemplate<Integer, String> template;
+
+   	Faker faker;
+
+   	@EventListener(ApplicationStartedEvent.class)
+   	public void generate() {
+
+    		faker = Faker.instance();
+     		final Flux<Long> interval = Flux.interval(Duration.ofMillis(1_000));
+
+    	final Flux<String> quotes = Flux.fromStream(Stream.generate(() -> faker.hobbit().quote()));
+
+     	Flux.zip(interval, quotes)
+      		.map(it -> template.send("hobbit", faker.random().nextInt(42), it.getT2())).blockLast();
+   	}
+ }
