1793ac50b46ac7510e0b3962077fa9ea6f69d31d
Satej <Satej>
2023-01-29 11:08:33 +0000

    Added kafka message consumer

--- src/main/java/io/confluent/developer/spring/SpringCcloudApplication.java
+++ src/main/java/io/confluent/developer/spring/SpringCcloudApplication.java
@@ -2,11 +2,13 @@ package io.confluent.developer.spring;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import com.github.javafaker.Faker;
 import org.springframework.boot.context.event.ApplicationStartedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Flux;
 
@@ -41,4 +43,13 @@ class Producer {
 		Flux.zip(interval, quotes)
 				.map(it -> template.send("hobbit", faker.random().nextInt(42), it.getT2())).blockLast();
 	}
+}
+
+@Component
+class Consumer {
+
+    @KafkaListener(topics = {"hobbit"}, groupId = "spring-boot-kafka")
+    public void consume(ConsumerRecord<Integer, String> record) {
+      System.out.println("received = " + record.value() + " with key " + record.key());
+    }
 }
