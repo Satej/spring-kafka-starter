913471e869ced0f8fcdc930981fcbb9008269691
Satej <Satej>
2023-01-29 04:25:04 +0000

    Added imports

--- src/main/java/io/confluent/developer/spring/SpringCcloudApplication.java
+++ src/main/java/io/confluent/developer/spring/SpringCcloudApplication.java
@@ -1,7 +1,17 @@
 package io.confluent.developer.spring;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.github.javafaker.Faker;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class SpringCcloudApplication {
@@ -12,23 +22,23 @@ public class SpringCcloudApplication {
 
 }
 
- @RequiredArgsConstructor
- @Component
- class Producer {
+@RequiredArgsConstructor
+@Component
+class Producer {
 
- 	private final KafkaTemplate<Integer, String> template;
+	private final KafkaTemplate<Integer, String> template;
 
-   	Faker faker;
+	Faker faker;
 
-   	@EventListener(ApplicationStartedEvent.class)
-   	public void generate() {
+	@EventListener(ApplicationStartedEvent.class)
+	public void generate() {
 
-    		faker = Faker.instance();
-     		final Flux<Long> interval = Flux.interval(Duration.ofMillis(1_000));
+		faker = Faker.instance();
+		final Flux<Long> interval = Flux.interval(Duration.ofMillis(1_000));
 
-    	final Flux<String> quotes = Flux.fromStream(Stream.generate(() -> faker.hobbit().quote()));
+		final Flux<String> quotes = Flux.fromStream(Stream.generate(() -> faker.hobbit().quote()));
 
-     	Flux.zip(interval, quotes)
-      		.map(it -> template.send("hobbit", faker.random().nextInt(42), it.getT2())).blockLast();
-   	}
- }
+		Flux.zip(interval, quotes)
+				.map(it -> template.send("hobbit", faker.random().nextInt(42), it.getT2())).blockLast();
+	}
+}
