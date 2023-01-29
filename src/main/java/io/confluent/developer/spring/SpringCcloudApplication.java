189562cebf0ebaff74f1a4fc5a35f7ce06e0b917
Satej <Satej>
2023-01-29 11:56:09 +0000

    Added stream processor to get word count

--- src/main/java/io/confluent/developer/spring/SpringCcloudApplication.java
+++ src/main/java/io/confluent/developer/spring/SpringCcloudApplication.java
@@ -1,8 +1,11 @@
 package io.confluent.developer.spring;
 
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.streams.StreamsBuilder;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import com.github.javafaker.Faker;
 import org.springframework.boot.context.event.ApplicationStartedEvent;
@@ -11,14 +14,24 @@ import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Flux;
+import org.apache.kafka.common.serialization.Serde;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.kstream.KTable;
+import org.apache.kafka.streams.kstream.Produced;
 
 import org.apache.kafka.clients.admin.NewTopic;
 import java.time.Duration;
 import java.util.stream.Stream;
+import org.springframework.kafka.annotation.EnableKafkaStreams;
 import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.config.TopicBuilder;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.Consumed;
+import org.apache.kafka.streams.kstream.Grouped;
+import java.util.Arrays;
 
-@SpringBootApplication
+@SpringBootApplication	
+@EnableKafkaStreams
 public class SpringCcloudApplication {
 
 	public static void main(String[] args) {
@@ -30,6 +43,10 @@ public class SpringCcloudApplication {
     	return TopicBuilder.name("hobbit2").partitions(12).replicas(3).build();
 	}
 
+	@Bean
+	NewTopic counts() {
+        return TopicBuilder.name("streams-wordcount-output").partitions(6).replicas(3).build();
+	}
 }
 
 @RequiredArgsConstructor
@@ -61,3 +78,22 @@ class Consumer {
       System.out.println("received = " + record.value() + " with key " + record.key());
     }
 }
+
+@Component
+class Processor {
+    @Autowired
+    public void process(StreamsBuilder builder) {
+		 final Serde<Integer> integerSerde = Serdes.Integer();
+		 final Serde<String> stringSerde = Serdes.String();
+		 final Serde<Long> longSerde = Serdes.Long();
+
+		 KStream<Integer, String> textLines = builder.stream("hobbit", Consumed.with(integerSerde, stringSerde));
+
+		 KTable<String, Long> wordCounts = textLines
+		    .flatMapValues(value -> Arrays.asList(value.toLowerCase().split("\\W+")))
+			.groupBy((key, value) -> value, Grouped.with(stringSerde, stringSerde))
+				    .count();
+
+        wordCounts.toStream().to("streams-wordcount-output", Produced.with(stringSerde, longSerde));
+      }
+  }
